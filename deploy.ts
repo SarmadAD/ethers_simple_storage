5926b5ec6bf8536de9dfe99fde1e9f28ca6843c9
Sarmad Ahmad <sarmadsmac@Sarmads-MacBook-Air.local>
2024-06-13 20:37:06 +0200

    add ts

--- deploy.ts
+++ deploy.ts
@@ -1,10 +1,10 @@
 import { JsonRpcProvider, Wallet, ContractFactory } from "ethers";
-import fs from "fs-extra";
+import * as fs from "fs-extra";
 import "dotenv/config";
 
 async function main() {
-    const provider = new JsonRpcProvider(process.env.RPC_URL);
-    const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
+    const provider = new JsonRpcProvider(process.env.RPC_URL!);
+    const wallet = new Wallet(process.env.PRIVATE_KEY!, provider);
     const abi = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.abi",
         "utf8"
@@ -15,8 +15,9 @@ async function main() {
     );
     const contractFactory = new ContractFactory(abi, binary, wallet);
     console.log("Deploying, please wait...");
-    const contract = await contractFactory.deploy();
-    await contract.deploymentTransaction().wait(1);
+    const contract: any = await contractFactory.deploy();
+    await contract.deploymentTransaction()?.wait(1);
+    console.log(contract);
 
     const currentFavoriteNumber = await contract.retrieve();
     console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
